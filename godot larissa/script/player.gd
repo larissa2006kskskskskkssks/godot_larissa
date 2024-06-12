2a24d84771f04e4f94bf8fd06b044339020679b8
larissa <130381560+larissa2006kskskskskkssks@users.noreply.github.com>
2024-06-12 09:38:38 -0300

    Add files via upload

--- godot larissa/script/player.gd
+++ godot larissa/script/player.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
-const SPEED : =  400
+const SPEED:= 400
+@onready var anim = $Anim
+@onready var collision = $collision
+@onready var screen_size = get_viewport_rect().size
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,5 +15,19 @@ func _process(delta):
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED 
-
+		
+	if velocity.x != 0:
+		anim.play("move")
+	elif velocity.y > 0:
+		anim.play ("move_up")
+	elif velocity.y < 0:
+		anim.play("move_down")
+	else:
+		anim.play("idle") 
+	if velocity.x > 0:
+		anim.flip_h= true
+	else:
+		anim.flip_h = false
+	
 	position += velocity * delta 
+	position = position.clamp(Vector2.ZERO,screen_size)
